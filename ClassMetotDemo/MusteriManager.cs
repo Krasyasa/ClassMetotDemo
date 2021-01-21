d60a39fa5887439bf7bb6c622f96c2d43d9f81e4
caganparmaksiz162005@gmail.com <caganparmaksiz162005@gmail.com>
2021-01-21 09:52:57 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Adi : " + musteri.Isim);
+            Console.WriteLine("Müşteri Soyadı : " + musteri.Soyad);
+            Console.WriteLine("Müşteri Bakiyesi : " + musteri.Bakiye);
+            Console.WriteLine("Kayıt Eklendi");
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Adi : " + musteri.Isim);
+            Console.WriteLine("Müşteri Soyadı : " + musteri.Soyad);
+            Console.WriteLine("Müşteri Bakiyesi : " + musteri.Bakiye);
+            Console.WriteLine("Kayıt Listesi");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Kayıt silindi");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteri = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Isim = "Çağan";
+            musteri1.Soyad = "Parmaksız";
+            musteri1.Bakiye = 2500;
+
+            Musteri musteri2 = new Musteri();
+            
+            musteri2.Id = 2;
+            musteri2.Isim = "Engin";
+            musteri2.Soyad = "Demiroğ";
+            musteri2.Bakiye = 3000;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteri.Add(musteri1);
+            Console.WriteLine("Müşteri 1 eklendi");
+            musteri.Add(musteri2);
+            Console.WriteLine("Müşteri 2 eklendi");
+            musteri.List(musteri1);
+            musteri.Delete(musteri1);
+
+            
+            
+            
+            
+
+        }
+    }
+}
